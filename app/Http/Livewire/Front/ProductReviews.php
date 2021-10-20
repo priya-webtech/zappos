ce0f1428e9602aae0bb45549d09fc982c0d3211f
vishalwebtech123 <85621500+vishalwebtech123@users.noreply.github.com>
2021-10-20 11:25:22 +0530

    product Review  : done

--- app/Http/Livewire/Front/ProductReviews.php
+++ app/Http/Livewire/Front/ProductReviews.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 
 class ProductReviews extends Component
 {
-	public $product_id,$productget;
+	public $product_id,$productget,$reviewget;
 
 	protected $rules = [
 
@@ -24,26 +24,23 @@ class ProductReviews extends Component
 
     public function mount($id)
     {
+        $this->user_id = Auth::user()->id;
     	$this->product_id = $id;
     	$this->productget = Product::where(['id' => $this->product_id])->first();
+        $this->reviewget = review::where(['product_id' => $this->product_id,'user_id' => $this->user_id])->first();
     }
 
     public function render()
     {
-    	$this->user_id = Auth::user()->id;
         return view('livewire.front.product-reviews');
     }
 
     public function SaveReview(Request $res)
     {
-
+        $this->user_id = Auth::user()->id;
     	$product = Product::where(['id' => $res->productid])->first();
-    	$this->user_id = Auth::user()->id;
-    	if($res->image){
-    		$path_url = $res->image->storePublicly('review','public');
-    	}else{
-    		$path_url = 'null';
-    	}
+        $productreview = review::where(['product_id' => $res->productid,'user_id' => $res->productid,])->first();
+        if(!empty($productreview)){
 
     	$review_arr = [
                     
@@ -66,11 +63,47 @@ class ProductReviews extends Component
                     'city'        => $res->city,
                     
                     'brand'       => $res->brand,
-
-                    'status'      => '1',
                 ];
 
         review::create($review_arr);
+        }
+        else{
+
+            if($res->image){
+            $path_url = $res->image->storePublicly('review','public');
+            }else{
+                $path_url = 'null';
+            }
+            review::where('id', $productreview['id'])->update(
+
+                [
+
+                    'product_id'  => $res->productid,
+                   
+                    'user_id'     => $this->user_id,
+                   
+                    'overall'     => $res->overall,
+                    
+                    'comfort'     => $res->comfort,
+                    
+                    'style'       => $res->style,
+                    
+                    'text'        => $res->text,
+
+                    'name'        => $res->name,
+                    
+                    'image'       => $path_url,
+                    
+                    'city'        => $res->city,
+                    
+                    'brand'       => $res->brand,                
+
+                ]
+
+            );
+
+             return redirect(route('product-front-detail', $product->seo_utl));
+        }
 
     }
 }
