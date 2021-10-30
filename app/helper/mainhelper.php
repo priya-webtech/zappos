e505a20e7b793ae727e09158edf98cf09b3e197f
vishalwebtech123 <85621500+vishalwebtech123@users.noreply.github.com>
2021-10-30 11:02:37 +0530

    select shipping

--- app/helper/mainhelper.php
+++ app/helper/mainhelper.php
@@ -1,6 +1,9 @@
 <?php 
 
 use App\Models\favorite;
+use App\Models\ShippingCharge;
+use App\Models\ProductVariant;
+use App\Models\Product;
 
   function favorite($product_id){
 
@@ -11,6 +14,29 @@ use App\Models\favorite;
   		return [ 'class' => 'add-wishlist', 'id' => $favorite['id'], 'product_id' => $favorite['product_id']];
   	}else{
   		return [ 'class' => '', 'id' => '0','product_id' => $product_id];
+  	}
+
+ }
+
+ function shipcharge($product_id,$checkproductname="blank"){
+
+  	$shipping = ShippingCharge::orderBy('id', 'asc')->first();
+
+  	if(!empty($shipping) && $checkproductname == 'variantproduct'){
+      $productprice = ProductVariant::where('product_id',$product_id)->first();
+  		if($productprice->price >= $shipping->maxcharge){
+        return [ 'label' => 'Ships Free!', 'shipprice' => '','price' => $productprice['price'],'compare_price' => $productprice['compare_price']];
+      }else{
+        return [ 'label' => 'shipping cost $', 'shipprice' => $shipping['minrate'],'price' => $productprice['price'],'compare_price' => $productprice['compare_price']];
+      }
+
+  	}else{
+      $productprice = Product::where('id',$product_id)->first();
+      if($productprice->price >= $shipping->maxcharge){
+  		  return [ 'label' => 'Ships Free!', 'shipprice' => '','price' => $productprice['price'],'compare_price' => $productprice['compare_price']];
+      }else{
+        return [ 'label' => 'shipping cost $', 'shipprice' => $shipping['minrate'],'price' => $productprice['price'],'compare_price' => $productprice['compare_price']];
+      }
   	}
 
  }
