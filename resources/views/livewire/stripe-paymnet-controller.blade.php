b733457960f8dfb7489dec7dec4f838b47c56c3e
vishalwebtech123 <85621500+vishalwebtech123@users.noreply.github.com>
2021-11-03 14:49:50 +0530

    check out page only insert

--- resources/views/livewire/stripe-paymnet-controller.blade.php
+++ resources/views/livewire/stripe-paymnet-controller.blade.php
@@ -52,33 +52,24 @@
 
 
 
-  
+<div class="checkout-page-sec">
 
 <div class="container">
 
   
 
-    <h1>Checkout<br/> </h1>
+    <h1 class="h1">Checkout<br/> </h1>
 
   
 
     <div class="row">
 
-        <div class="col-md-6 col-md-offset-3">
+        <div class="col-md-8">
 
-            <div class="panel panel-default credit-card-box">
+            <div class="panel credit-card-box">
 
-                <div class="panel-heading display-table" >
 
-                    <div class="row display-tr" >
-
-                        <h3 class="panel-title display-td" >Shipping Details</h3>
-
-                    </div>                    
-
-                </div>
-
-                <div class="panel-body" wire:ignore>
+                <div class="checkout-left" wire:ignore>
 
   
 
@@ -94,96 +85,153 @@
 
                     @endif
                     @if($view == 'address')
-                    <form role="form" id="address-form" class="require-validation"  >
-                        
-                        <input type="hidden" name="orderid" value="{{$orderdetail->id}}">
-                        <div class='form-row row'>
 
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label'>Full name</label> 
-                                <input class='form-control' wire:model="fullname" name="fullname" type='text'>
-                            </div>
-                            <div class='col-xs-12 form-group'>
-                                <label class='control-label'>Address</label> 
-                                <textarea class='form-control' wire:model="address" name="address" type='text'></textarea>
-                            </div>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label'>City</label> 
-                                <input class='form-control' wire:model="city" name="city" type='text'>
-                            </div>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label'>Country</label> 
-                                <input class='form-control' wire:model="country" name="country" type='text'>
-                            </div>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label'>Pincode</label> 
-                                <input class='form-control' wire:model="pincode" name="pincode" type='text'>
-                            </div>
-                            <div class='col-xs-12 form-group required'>
-                                <label class='control-label'>Mobile Number</label> 
-                                <input class='form-control' wire:model="mobile" name="mobile" type='text'>
-                            </div>
-                        </div>
+                    <div class="shipping-details-card">
 
-                        <div class='form-row row'>
+                        <h3 class="panel-title">Shipping Details</h3>
 
-                            <div class='col-md-12 error form-group hide'>
+                        <form role="form" id="address-form" class="require-validation"  >
+                            
+                            <input type="hidden" name="orderid" value="{{$orderdetail->id}}">
+                            <div class='form-row'>
 
-                                <div class='alert-danger alert'>Please correct the errors and try
-
-                                    again.</div>
-
+                                <div class='col-xs-12 form-group required'>
+                                    <label class='control-label'>Full name</label> 
+                                    <input class='form-control' wire:model="fullname" name="fullname" type='text'>
+                                </div>
+                                <div class='col-xs-12 form-group'>
+                                    <label class='control-label'>Address</label> 
+                                    <textarea class='form-control' wire:model="address" name="address" type='text'></textarea>
+                                </div>
+                                <div class='col-xs-12 form-group required'>
+                                    <label class='control-label'>City</label> 
+                                    <input class='form-control' wire:model="city" name="city" type='text'>
+                                </div>
+                                <div class='col-xs-12 form-group required'>
+                                    <label class='control-label'>Country</label> 
+                                    <input class='form-control' wire:model="country" name="country" type='text'>
+                                </div>
+                                <div class='col-xs-12 form-group required'>
+                                    <label class='control-label'>Pincode</label> 
+                                    <input class='form-control' wire:model="pincode" name="pincode" type='text'>
+                                </div>
+                                <div class='col-xs-12 form-group required'>
+                                    <label class='control-label'>Mobile Number</label> 
+                                    <input class='form-control' wire:model="mobile" name="mobile" type='text'>
+                                </div>
                             </div>
 
-                        </div>
+                            <div class='form-row'>
 
-                        
+                                <div class='col-md-12 error form-group hide'>
 
-                        <div class="row">
+                                    <div class='alert-danger alert'>Please correct the errors and try
 
-                            <div class="col-xs-12">
-                              
-                                <button class="btn btn-primary btn-lg btn-block" wire:click.prevent="addshipping({{$orderdetail->id}})" >Submit</button>
+                                        again.</div>
+
+                                </div>
 
                             </div>
 
-                        </div>
+                            
 
-                          
+                            <div class="row">
 
-                    </form>
+                                <div class="col-xs-12">
+                                  
+                                    <button class="site-btn" wire:click.prevent="addshipping({{$orderdetail->id}})" >Submit</button>
+
+                                </div>
+
+                            </div>
+
+                        </form>
+                    </div>
                     @endif
-  
 
-                      <form id="payment-form">
-                        @csrf
-                        <h3>Payment</h3>
-                        <label for="name">
-                          Account Holder Name
-                        </label>
-                        <input id="acholdername" value="" required>
-
-                        <label for="ideal-bank-element">
-                          iDEAL Bank
-                        </label>
-                        <div id="ideal-bank-element">
-                          <!-- A Stripe Element will be inserted here. -->
+                    <div class="review-shipping-sec">
+                        <h3 class="panel-title">Shipping Details</h3>
+                        <div class="my-cart-pd-details">
+                            <div class="my-cart-img">
+                                <a class="dropdown-header" href="#">
+                                    <img src="http://127.0.0.1:8000/storage/media/nU6wmGj0C1dy5DUk13yDMuEnqNSo4uaNWZktAb0L.jpg">
+                                </a>
+                            </div>
+                            <div class="my-cart-desc">
+                                <span>Splendid</span>
+                                <h6>1x1 Classic Long Sleeve Turtleneck</h6>
+                                <p>
+                                    <span><b>Color:</b> White</span>
+                                    <span><b>Size:</b> MD (Women's 6-8)</span>
+                                </p>
+                                <p>
+                                    <span><b>Sale:</b> <span class="red-color">$44.95</span></span>
+                                    <span class="grey-color"><b>MSRP:</b> $58.00</span>
+                                </p>
+                            </div>
                         </div>
-
-                        <button type="submit">Pay {{$orderdetail->netamout}}</button>
-
-                        <!-- Used to display form errors. -->
-                        <div id="error-message" role="alert"></div>
-                      </form>
-                   
-
-                      <div id="messages" role="alert" style="display: none;"></div>
-                   
-
+                        <div class="my-cart-pd-details">
+                            <div class="my-cart-img">
+                                <a class="dropdown-header" href="#">
+                                    <img src="http://127.0.0.1:8000/storage/media/nU6wmGj0C1dy5DUk13yDMuEnqNSo4uaNWZktAb0L.jpg">
+                                </a>
+                            </div>
+                            <div class="my-cart-desc">
+                                <span>Splendid</span>
+                                <h6>1x1 Classic Long Sleeve Turtleneck</h6>
+                                <p>
+                                    <span><b>Color:</b> White</span>
+                                    <span><b>Size:</b> MD (Women's 6-8)</span>
+                                </p>
+                                <p>
+                                    <span><b>Sale:</b> <span class="red-color">$44.95</span></span>
+                                    <span class="grey-color"><b>MSRP:</b> $58.00</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+  
                 </div>
 
             </div>        
 
+        </div>
+
+        <div class="col-md-4">
+            <div class="viewcart-checkout">
+                <div class="vc-inner">
+                    <p class="cart-summary">Order Summary (3 Items)</p>
+                    <p class="subtotal">subtotal:<span>$334.85</span></p>
+                    <p class="subtotal">Shipping Cost:<span>Free</span></p>
+                    <p class="discount-price">discount: <span>-$10.00</span></p>
+                    <p class="subtotal">Total before tax:<span>$324.85</span></p>
+                    <p class="subtotal">Estimated tax to be collected:*<span>$0.00</span></p>
+                </div>
+                <div class="vc-inner">
+                    <p class="total-price">total: <span>$324.85</span></p>
+                </div>
+            </div>
+            <form id="payment-form">
+                @csrf
+                <h3 class="panel-title">Payment</h3>
+                <label for="name"> Account Holder Name</label>
+                <div class="account-name-row">
+                    <input id="acholdername" value="" required>
+                    <label for="ideal-bank-element" class="bank-name">iDEAL Bank</label>
+                </div>
+                <div class="account-name-row">
+                    <div id="ideal-bank-element">
+                      <!-- A Stripe Element will be inserted here. -->
+                    </div>
+                    <button type="submit">Pay {{$orderdetail->netamout}}</button>
+                </div>
+                <!-- Used to display form errors. -->
+                <div id="error-message" role="alert"></div>
+              </form>
+           
+
+              <div id="messages" role="alert" style="display: none;"></div>
+            <p class="privacy-condi-text">By placing your order, you agree to company-name.com’s <a href="#">privacy notice</a> and <a href="#">conditions of use</a>.</p>
         </div>
 
     </div>
@@ -192,8 +240,7 @@
 
 </div>
 
-  
-
+</div>
 
   
 
