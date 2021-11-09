c6a78e3bf6a9e5c2dd82a740acf8b21557bcb29d
vishalwebtech123 <85621500+vishalwebtech123@users.noreply.github.com>
2021-11-09 16:15:06 +0530

    09-11-2021 code update

--- app/Http/Livewire/Discount/DiscountCreate.php
+++ app/Http/Livewire/Discount/DiscountCreate.php
@@ -4,10 +4,59 @@ namespace App\Http\Livewire\Discount;
 
 use Livewire\Component;
 
+use App\Models\discount;
+
+use str;
+
 class DiscountCreate extends Component
 {
+	public $applyto,$type,$code,$discount_value,$randomString,$start_date,$end_date,$start_time,$end_time;
+
+	protected $listeners = ['SaveRecord'];
     public function render()
     {
+
         return view('livewire.discount.discount-create');
+    }
+
+    public function RendomGenrate()
+    {
+    	$characters = '1234567890ABCDEFGHIJKLMNOPQRSTUVWYZ';
+    	$charactersLength = strlen($characters);
+        $this->randomString = '';
+        for ($i = 0; $i < 10; $i++) {
+            $this->randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        $this->code = $this->randomString;
+    }
+
+    public function SaveRecord($flag)
+    {
+
+    	if($flag == 'save-discount')
+        {
+
+            $this->validate([
+                'code' => 'required',
+                'discount_value' => 'required'
+            ]);
+
+             discount::insert([
+             	'code' => $this->code,
+             	'type' => $this->type,
+             	'discount_value' => $this->discount_value,
+             	'applyto' => $this->applyto,
+             	'start_date' => $this->start_date,
+             	'start_time' => $this->start_time,
+             	'end_date' => $this->end_date,
+             	'end_time' => $this->end_time,
+             	'status' => '1',
+             ]);
+
+             session()->flash('message', 'Discount Created Successfully.');
+             return redirect(route('discount-list'));
+            //  $this->resetInput();
+        }
+
     }
 }
--- database/migrations/2021_11_08_125142_create_discounts_table.php
+++ database/migrations/2021_11_08_125142_create_discounts_table.php
@@ -15,6 +15,16 @@ class CreateDiscountsTable extends Migration
     {
         Schema::create('discounts', function (Blueprint $table) {
             $table->id();
+            $table->string('code');
+            $table->Integer('type');
+            $table->bigInteger('discount_value');
+            $table->text('applyto');
+            $table->text('apply_c_p');
+            $table->date('start_date');
+            $table->time('start_time');
+            $table->date('end_date');
+            $table->time('end_time');
+            $table->string('status');
             $table->timestamps();
         });
     }
