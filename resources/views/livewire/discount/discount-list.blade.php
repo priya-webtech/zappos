2d6507f38bc74a717028f6a8e80ad36dffa6ae61
Lalit  prajapati <lalit@webtech-evolution.com>
2021-11-12 20:01:34 +0530

    discount-page design

--- resources/views/livewire/discount/discount-list.blade.php
+++ resources/views/livewire/discount/discount-list.blade.php
@@ -22,18 +22,17 @@
                     <article class="full-width">
                         <div class="columns" wire:ignore="">
                             <div class="input-group">
+                                <div class="dropdown-field sort-btn ml-0 mr-04">
+                                    <label id="main_tagged_with" class="dropdown-label" onclick="openOption('main_tagged_with')">
+                                        Filter
+                                        <svg viewBox="0 0 20 20" class=" Polaris-Icon__Svg_375hu" focusable="false" aria-hidden="true">
+                                            <path d="m5 8 5 5 5-5H5z"></path>
+                                        </svg>
+                                    </label>
+                                </div>
                                 <!-- Search Field -->
                                 <div class="search-product-field">
                                     <input class="fs-13 placeholder_gray fw-4" type="search" name="search_products" wire:model="filter_customers" id="search_products" placeholder="Filter Discount">
-                                </div>
-                                
-                                <div class="sort-btn">
-                                    <button class="secondary fw-6" id="sort">
-                                        <svg viewBox="0 0 20 20" class="Polaris-Icon__Svg_375hu" focusable="false" aria-hidden="true">
-                                            <path d="M5.293 2.293a.997.997 0 0 1 1.414 0l3 3a1 1 0 0 1-1.414 1.414L7 5.414V13a1 1 0 1 1-2 0V5.414L3.707 6.707a1 1 0 0 1-1.414-1.414l3-3zM13 7a1 1 0 0 1 2 0v7.585l1.293-1.292a.999.999 0 1 1 1.414 1.414l-3 3a.997.997 0 0 1-1.414 0l-3-3a.997.997 0 0 1 0-1.414.999.999 0 0 1 1.414 0L13 14.585V7z"></path>
-                                        </svg>
-                                        Sort
-                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -48,11 +47,79 @@
                     <table class="one-bg border-every-row fs-14 fw-3 table-padding-side0 tc-black01 comman-th product-listing collections-sec">
                         <tbody>
                             <tr>
-                            	<th>#</th>
-                                <th>Code</th>
-                                <th>Start Date</th>
-                                <th>End Date</th>
+                                <th>
+                                    <div class="row all-select-checkbox"><label><input type="checkbox" class="checked_all" name="customer_all"></label></div>
+                                </th>
+                                <th class="fw-6">
+                                    <span class="all-customer-list pl-3">
+                                        Showing 1 discount code
+                                    </span>
+                                    <div class="select-customers" style="display: none;">
+                                        <span class="button-group product-more-action">
+                                            <button class="secondary all-customer-select">
+                                                 <input type="checkbox" class="checked_all_clone" name="customer_all">
+                                                <span><span class="selected_count">0</span> selected</span>
+                                            </button>
+                                            <button class="secondary select-customer-more-actions" onclick="openOption('email_subscription_status')">
+                                                Actions
+                                                <svg viewBox="0 0 20 20" class=" Polaris-Icon__Svg_375hu" focusable="false" aria-hidden="true"><path d="m5 8 5 5 5-5H5z"></path></svg>
+                                            </button>
+                                            <ul class="filter_email_subscription_status filter-dropdown more-action-dropdown" style="list-style-type: none" wire:ignore.self="">
+                                                <li>
+                                                    <button class="link">Enable discount codes</button>
+                                                </li>
+                                                <li>
+                                                    <button class="link">Disable discount codes</button>
+                                                </li>
+                                                <li>
+                                                    <button class="link">Delete discount codes</button>
+                                                </li>
+                                                <li>
+                                                    <button class="link">Delete products</button>
+                                                </li>
+                                            </ul>
+                                        </span>
+                                    </div>
+                                </th>
+                                <th></th>
+                                <th></th>
+                                <th class="ta-right">
+                                    <div class="inventory-shortby one-bg border-every-row d-inline-block mr-3">
+                                        <div class="sort-by-field">
+                                            <label>Sort by</label>
+                                            <select class="resizeselect" style="width: 180px;">
+                                                    <option value="id desc">Last created</option>
+                                                    <option value="title asc">Discount code (A-Z)</option>
+                                                    <option value="title desc">Discount code (Z-A)</option>
+                                                    <option value="starts_at asc">Start date (ascending)</option>
+                                                    <option value="starts_at desc">Start date (descending)</option>
+                                                    <option value="ends_at asc">End date (ascending)</option>
+                                                    <option value="ends_at desc">End date (descending)</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </th>
                             </tr>
+                            <tr>
+                                <td>
+                                    <div class="row"><label><input type="checkbox" class="checkbox" name="selectedproducts" value="117" wire:model.lazy="selectedproducts"></label></div>
+                                </td>
+                                <td class="product-table-item">
+                                    <a class="tc-black fw-6">T31JX0W377CG</a>
+                                    <p class="mb-0">10% off Home page • Minimum purchase of $20.00 • One use per customer</p>
+                                </td>
+                                <td >
+                                    <p class="tag green order-filed"> Active </p> 
+                                </td>
+                                <td>
+                                    0/10 used
+                                </td>
+                                <td class="ta-right">
+                                    <span class="text-grey  pr-3">From 6 Nov 20</span>
+                                </td>
+                            </tr>
+
+
                             @if(!empty($discountlist))
                             @foreach($discountlist as $row)
                             <tr>
