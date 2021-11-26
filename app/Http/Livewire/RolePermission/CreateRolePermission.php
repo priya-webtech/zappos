93d5ceb79fff025dfa9962678f7311c3a09b1c8b
vishalwebtech123 <85621500+vishalwebtech123@users.noreply.github.com>
2021-11-26 19:07:12 +0530

    comiit  26-11-21

--- app/Http/Livewire/RolePermission/CreateRolePermission.php
+++ app/Http/Livewire/RolePermission/CreateRolePermission.php
@@ -3,11 +3,28 @@
 namespace App\Http\Livewire\RolePermission;
 
 use Livewire\Component;
+use App\Models\rolepermission;
 
 class CreateRolePermission extends Component
 {
+
+	public $Permission = [];
+
     public function render()
     {
         return view('livewire.role-permission.create-role-permission');
+    }
+
+    public function Permissionsave(){
+    	dd($Permission);
+    	foreach ($Permission as $key => $value) {
+    		rolepermission::create([
+                'menu_id' => '2',
+
+                'status	' => $value,
+            ]);
+
+            return redirect(route('role-permission'));
+    	}
     }
 }
